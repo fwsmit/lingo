cb656e46385eaeaabba2c5e2a340ca4b66c1a541
Nathan <N.P.A.Kindt@student.tudelft.nl>
2023-08-16 21:10:21 +0200

    OWee Blikbier Lingo

--- Lingo.py
+++ Lingo.py
@@ -8,19 +8,21 @@ import pygame as pg
 
 
 WORD_LENGTH = 6
-BEEP_DELAY = 0.23
+BEEP_DELAY = 0.20
 BG_COLOR = "#0A2E58"
 FG_COLOR = "#0952BB"
 RED_COLOR = "#d33038"
 YELLOW_COLOR = "#d4b83d"
-PADDING = 2
+PADDING = 3
+d = 1610 / (PADDING + 6)
 
 with open("targets.txt") as file:
     DATABASE = [word.strip() for word in file]
+    DATABASE = [word.replace("ij", chr(131)) for word in DATABASE]
 
 
 def color_code(guess, word):
-    colors = ["R", "B", "B", "B", "B", "B"]
+    colors = ["B", "B", "B", "B", "B", "B"]
     check_word = word.copy()
     for i in range(len(check_word)):
         if check_word[i] == guess[i]:
@@ -28,10 +30,9 @@ def color_code(guess, word):
             check_word[i] = "*"
 
     for i in range(len(check_word)):
-        if check_word[i] != "*":
-            if guess[i] in check_word:
-                check_word[i] = "*"
-                colors[i] = "Y"
+        if check_word[i] != "*" and guess[i] in check_word:
+            check_word[check_word.index(guess[i])] = "*"
+            colors[i] = "Y"
     return colors
 
 
@@ -42,8 +43,8 @@ def draw_letter(root, row, column, color, text):
     )
     c = tk.Canvas(
         root,
-        width=160,
-        height=160,
+        width=d,
+        height=d,
         bg=FG_COLOR if color == "X" else BG_COLOR,
         highlightthickness=0,
         highlightbackground=BG_COLOR,
@@ -51,15 +52,15 @@ def draw_letter(root, row, column, color, text):
     )
     c.grid(row=row, column=column)
     if color == "Y":
-        c.create_oval(3, 3, 159, 159, fill=YELLOW_COLOR, outline="")
-        c.create_text(80, 80, text=text, fill="white", font=font)
-        border_color.grid(row=row, column=column)
+        c.create_oval(3, 3, d, d, fill=YELLOW_COLOR, outline="")
     else:
         c.create_rectangle(
-            3, 3, 159, 159, fill=RED_COLOR if color == "R" else BG_COLOR, outline=""
+            3, 3, d, d, fill=RED_COLOR if color == "R" else BG_COLOR, outline=""
         )
-        c.create_text(80, 80, text=text, fill="white", font=font)
-        border_color.grid(row=row, column=column)
+    c.create_text(
+        d / 2, d / 2, text=text.replace(chr(131), "ij"), fill="white", font=font
+    )
+    border_color.grid(row=row, column=column)
 
 
 def show_word(root, word, color_code, row, mute, delay):
@@ -85,8 +86,8 @@ def pad_grid(root, row):
     for i in range(0, PADDING):
         c = tk.Canvas(
             root,
-            width=180,
-            height=160,
+            width=d - 20,
+            height=d,
             bg=BG_COLOR,
             highlightthickness=0,
             highlightbackground=BG_COLOR,
@@ -115,45 +116,64 @@ class Lingo(threading.Thread):
         self.root.mainloop()
 
 
-pg.mixer.init()
 pg.init()
+pg.mixer.init()
 red_beep = pg.mixer.Sound("sound/Beep (goed).wav")
 blue_beep = pg.mixer.Sound("sound/Beep (fout).wav")
 yellow_beep = pg.mixer.Sound("sound/Beep (half goed).wav")
-victory = pg.mixer.Sound("sound/Lingo Goed Word.mp3")
 
-pg.mixer.set_num_channels(8)
 
-app = Lingo()
-time.sleep(1)
-app.root.update()
-while True:
-    used_words = []
-    word = [x for x in DATABASE.pop(random.randint(0, len(DATABASE)-1))]
-    guess = [word[0], ".", ".", ".", ".", "."]
-    turn = 1
-    print("Answer:", "".join(word))
-    show_word(
-        app.root, guess, ["R", "B", "B", "B", "B", "B"], turn, mute=True, delay=True
-    )
+def game():
+    victory = pg.mixer.Sound("sound/Lingo Goed Word.mp3")
+    pg.mixer.set_num_channels(8)
+    app = Lingo()
+    time.sleep(1)
     app.root.update()
     while True:
-        while True:
-            guess = input("Guess: ")
-            if len(guess) != 6:
-                print(f"dat woord is niet 6 lang, foei. ga je schamen")
-            else:
-                break
-        colors = color_code(guess, word)
-        show_word(app.root, guess, colors, turn, False, True)
-        turn += 1
-        if all(x == "R" for x in colors):
-            print("CORRECT")
-            victory.play()
-            print("Press enter to play again")
-            input()
-            break
-        w = "".join([word[i] if colors[i] == "R" else "." for i in range(len(guess))])
-        show_word(app.root, w, color_code(w, word), turn, True, False)
+        used_words = []
+        word = [x for x in DATABASE.pop(random.randint(0, len(DATABASE) - 1))]
+        red_word = [word[0], ".", ".", ".", ".", "."]
+        guess = red_word.copy()
+        turn = 1
+        print("Answer:", ("".join(word)).replace(chr(131), "ij"))
+        show_word(
+            app.root, guess, color_code(guess, word), turn, mute=True, delay=False
+        )
         app.root.update()
-    clear(app.root)
+        while True:
+            while True:
+                guess = input("Guess: ")
+                guess = guess.replace("ij", chr(131)).replace("ij", chr(131))
+                if len(guess) != 6:
+                    print(f"dat woord is niet 6 lang, foei. ga je schamen")
+                else:
+                    break
+            colors = color_code(guess, word)
+            red_word = [
+                guess[i] if colors[i] == "R" else red_word[i] for i in range(len(guess))
+            ]
+            # show_word(app.root, ["", "", "", "", "", ""], ["B", "B", "B", "B", "B", "B"], turn, True, False)
+            # time.sleep(0.1)
+            show_word(app.root, guess, colors, turn, False, True)
+            turn += 1
+            if all(x == "R" for x in colors):
+                print("CORRECT")
+                victory.play()
+                print("Press enter to play again")
+                input()
+                break
+            show_word(
+                app.root, red_word, ["B", "B", "B", "B", "B", "B"], turn, True, False
+            )
+            if turn == 7:
+                print("press enter to show answer")
+                input()
+                show_word(app.root, word, ["R", "R", "R", "R", "R", "R"], turn-1, False, True)
+                print("Press enter to play again")
+                input()
+                break
+            # app.root.update()
+        clear(app.root)
+
+
+game()
